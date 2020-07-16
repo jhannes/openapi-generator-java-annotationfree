30cca491569168104171f2be54aa8a576ef559a0
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-07-16 23:30:17 +0200

    Initial commit

---
+++ src/main/java/io/github/jhannes/openapi/javaannotationfree/JavaCodegen.java
@@ -0,0 +1,11 @@
+package io.github.jhannes.openapi.javaannotationfree;
+
+import org.openapitools.codegen.languages.JavaClientCodegen;
+
+public class JavaCodegen extends JavaClientCodegen {
+
+    @Override
+    public String getName() {
+        return "java-annotationfree";
+    }
+}
